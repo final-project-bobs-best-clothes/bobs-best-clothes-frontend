8fa28410a200c824ec2a39f64402aee1999394d4
hanna <hannaphamm1007@gmail.com>
2024-03-12 15:57:13 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 style={{textAlign:'center'}}>Hello world!</h1>
+      <div className="container text-center">
+        <div className="row align-items-start">
+          <div className="col">One of three columns</div>
+          <div className="col">One of three columns</div>
+          <div className="col">One of three columns</div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
