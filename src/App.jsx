58a7a3e0bcb431270abe6136cdcc7cdf884e567c
Axel Hansson <axel.co.hansson@gmail.com>
2024-03-13 14:38:28 +0100

    test commit

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,9 @@ function App() {
       <h1 style={{textAlign:'center'}}>Hello world!</h1>
       <div className="container text-center">
         <div className="row align-items-start">
-          <div className="col">One of three columns</div>
-          <div className="col">One of three columns</div>
-          <div className="col">One of three columns</div>
+          <div className="col">First column</div>
+          <div className="col">Second column</div>
+          <div className="col">Third column</div>
         </div>
       </div>
     </>
