95806a00c89ab7acd5352c92461741a3478d8feb
hanna <hannaphamm1007@gmail.com>
2024-03-20 13:52:25 +0100

    added styling

--- src/components/ProductFeed/ProductFeed.jsx
+++ src/components/ProductFeed/ProductFeed.jsx
@@ -1,37 +1,31 @@
-import  { useContext, useState } from 'react'
-import ProductList from './components/ProductList'
-import SearchBar from '../SearchBar/SearchBar'
-import { CategoryContext } from '../../App';
-import SideBar from '../SideBar/SideBar';
-import "./../../App.css"
+import { useContext, useState } from "react";
+import ProductList from "./components/ProductList";
+import SearchBar from "../SearchBar/SearchBar";
+import { CategoryContext } from "../../App";
+import SideBar from "../SideBar/SideBar";
+import "./../../App.css";
 
 function ProductFeed() {
-    const {setFilterCategory, filterCategory} = useContext(CategoryContext)
-    const [filterText, setFilterText] = useState('');
+  const { setFilterCategory, filterCategory } = useContext(CategoryContext);
+  const [filterText, setFilterText] = useState("");
 
-    return (
-        <div className="container-fluid pt-5">
-        <div className="row">
-            {/* Sidebar */}
-            <div className="col-md-2 sidebar position-fixed vh-100">
-                <SideBar setFilterCategory={setFilterCategory} />
-            </div>
-            {/* Main Content */}
-            <div className="col-md-10 d-flex justify-content-center align-items-center offset-md-2">
-                <div className="row">
-                    {/* Search Bar */}
-                    <div className="col-md-12 text-center mt-5">
-                        <SearchBar filterText={filterText} setFilterText={setFilterText} />
-                    </div>
-                    {/* Product List */}
-                    <div className="col-md-12 p-4">
-                        <ProductList filterText={filterText} filterCategory={filterCategory} />
-                    </div>
-                </div>
-            </div>
+  return (
+    <div className="container pt-5" style={{ marginTop: "50px" }}>
+      <div className="row align-items-center justify-content-center">
+        <div className="col-md-10 col-12">
+          <SearchBar filterText={filterText} setFilterText={setFilterText} />
         </div>
+        <div className="col-md-2 col-12">
+          <SideBar setFilterCategory={setFilterCategory}></SideBar>
+        </div>
+
+      </div>
+      {/* Product List */}
+      <div className="col-md-12 p-4">
+        <ProductList filterText={filterText} filterCategory={filterCategory} />
+      </div>
     </div>
-    )
+  );
 }
 
 export default ProductFeed;
--- src/components/Profile/components/OrderList.jsx
+++ src/components/Profile/components/OrderList.jsx
@@ -3,8 +3,19 @@ import { useEffect, useContext, useState } from "react";
 import OrderItem from "./OrderItem";
 
 function OrderList() {
-  const {orders, setOrders} = useContext(OrderContext);
-  
+  const { orders, setOrders } = useContext(OrderContext);
+  const reverseOrders = [...orders].reverse();
+  const [currentPage, setCurrentPage] = useState(1);
+  const ordersPerPage = 3;
+
+  const indexOfLastOrder = currentPage * ordersPerPage;
+  const indexOfFirstOrder = indexOfLastOrder - ordersPerPage;
+  const currentOrders = reverseOrders.slice(indexOfFirstOrder, indexOfLastOrder);
+
+  const handlePageClick = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
   const fetchOrders = () => {
     fetch("http://localhost:4000/orders")
       .then((res) => res.json())
@@ -18,22 +29,38 @@ function OrderList() {
     fetchOrders();
   }, []);
 
-
   return (
     <div className="container">
-      {orders.map((order, index) => (
+      {currentOrders.map((order, index) => (
         <div key={index} className="row justify-content-center mb-4">
           <div className="col-md-6">
-            <div className="border rounded p-3" >
-              <h5 className="mb-4" style={{fontWeight:"bold"}}>Order Number: {order.id}</h5>
+            <div className="border rounded p-3">
+              <h5 className="mb-4" style={{ fontWeight: "bold" }}>
+                Order Number: {order.id}
+              </h5>
               <OrderItem order={order} />
-              <p className= "text-center"style={{fontWeight:'bold'}}>Total: {"$" +order.total}</p>
+              <p className="text-center" style={{ fontWeight: "bold" }}>
+                Total: {"$" + order.total}
+              </p>
             </div>
           </div>
         </div>
       ))}
+      {reverseOrders.length > ordersPerPage && (
+        <nav className="d-flex justify-content-center">
+          <ul className="pagination">
+            {[...Array(Math.ceil(reverseOrders.length / ordersPerPage)).keys()].map((pageNumber) => (
+              <li key={pageNumber} className={`page-item ${currentPage === pageNumber + 1 ? 'active' : ''}`}>
+                <button className="page-link" onClick={() => handlePageClick(pageNumber + 1)}>
+                  {pageNumber + 1}
+                </button>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      )}
     </div>
-  )
+  );
 }
 
-export default OrderList
+export default OrderList;
